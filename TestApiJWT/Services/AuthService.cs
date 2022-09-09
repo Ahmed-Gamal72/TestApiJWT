f59faddb761e7b2d6ffe7066845f0fb65c478ef8
Ahmed Gad <ahmedgad1497@gmail.com>
2022-09-09 18:03:31 +0200

    Final : Add services that serve login and addroles Endpoints

--- TestApiJWT/Services/AuthService.cs
+++ TestApiJWT/Services/AuthService.cs
@@ -17,13 +17,16 @@ namespace TestApiJWT.Services
     public class AuthService : IAuthService
     {
         private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
         private readonly JWT _jwt;
 
-        public AuthService(UserManager<ApplicationUser> userManager,IOptions<JWT> jwt)
+        public AuthService(UserManager<ApplicationUser> userManager, IOptions<JWT> jwt, RoleManager<IdentityRole> roleManager)
         {
             _userManager = userManager;
             _jwt = jwt.Value;
+            _roleManager = roleManager;
         }
+
 
         public async Task<AuthModel> RegisterAsync(RegisterModel model)
         {
@@ -69,6 +72,43 @@ namespace TestApiJWT.Services
             };
         }
 
+        public async Task<AuthModel> GetTokenAsync(TokenRequestModel model)
+        {
+            var authModel = new AuthModel();
+
+            var user = await _userManager.FindByEmailAsync(model.Email);
+            if (user == null || !await _userManager.CheckPasswordAsync(user, model.Password))
+            {
+                authModel.Message = "Email or password is incorrect!";
+                return authModel;   
+            }
+
+            var jwtSecurityToken = await CreateJwtToken(user);
+            var rolesList = await _userManager.GetRolesAsync(user);
+
+            authModel.IsAuthenticated=true;
+            authModel.Token= new JwtSecurityTokenHandler().WriteToken(jwtSecurityToken);
+            authModel.Email=user.Email;
+            authModel.UserName=user.UserName;
+            authModel.ExpiresOn=jwtSecurityToken.ValidTo;
+            authModel.Roles = rolesList.ToList();
+
+            return authModel;
+        }
+
+        public async Task<string> AddRoleAsync(AddRoleModel model)
+        {
+            var user = await _userManager.FindByIdAsync(model.UserId);
+            if (user is null || !await _roleManager.RoleExistsAsync(model.Role))
+                return "Invalid user ID or Role";
+
+            if (await _userManager.IsInRoleAsync(user, model.Role))
+                return "user already assigned to this role";
+             
+            var result= await _userManager.AddToRoleAsync(user,model.Role);
+
+            return result.Succeeded ? String.Empty : "somtihng went wrong";
+        }
 
         private async Task<JwtSecurityToken> CreateJwtToken(ApplicationUser user)
         {
@@ -101,5 +141,7 @@ namespace TestApiJWT.Services
 
             return jwtSecurityToken;
         }
+
+
     }
 }
--- TestApiJWT/Services/IAuthService.cs
+++ TestApiJWT/Services/IAuthService.cs
@@ -6,5 +6,7 @@ namespace TestApiJWT.Services
     public interface IAuthService
     {
         Task<AuthModel> RegisterAsync(RegisterModel model);
+        Task<AuthModel> GetTokenAsync(TokenRequestModel model);
+        Task<string> AddRoleAsync(AddRoleModel model);
     }
 }
